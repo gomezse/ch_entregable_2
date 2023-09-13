578eb9b581f9b63bd938bb04eea3a797944ced05
gomezse <gomezsebastian909@gmail.com>
2023-09-13 16:49:11 -0300

    entregable 2 finalizado

--- main/ProductManager.js
+++ main/ProductManager.js
@@ -1,13 +1,7 @@
-/*Clase que tiene como fin gestionar un conjunto de productos.
-Restricciones:
--Debe tener campo "id" automatico, unique y autoincremental.
--El campo "code" debe ser unique.
--Todos los campos son obligatorios
-*/
+// import para hacer uso de File System nativo de node
 const fs = require('fs');
 
-const path='';
-// El resto de tu código aquí...
+
 
 class ProductManager {
 
@@ -22,82 +16,62 @@ class ProductManager {
 
     validate = (title, description, price, thumbnail, code, stock) => title && description && price && thumbnail && code && stock;
 
-    /**
-     * @param {*} code codigo del producto a buscar en la lista de productos.
-     * @returns devuelve el objeto en caso de coincidir el code
-     */
+    //Devolver true en caso de encontrar un objeto con el code, caso contrario false.
+    async existCode(code, path) {
+        const products = await this.getProducts(path);
+        const existingProduct = products.find(product => product.code === code);
 
-    existCode = (code) => this.products.find(product => product.code === code);
+        return Boolean(existingProduct);
+    }
 
-    /**
-     * @param {*} searchId del producto a buscar en la lista de productos
-     * @returns objeto o undefinded
-     */
+    //Obtener un objeto del archivo mediante su id.
+    async getProductById(searchId, path) {
+        const products = await this.getProducts(path);
+        const product = products.find(product => product.id === searchId);
 
-    
-    async getProduct(searchId){
-        const products= await this.getProducts();
-        const product = products.find(product=>product.id === searchId);
-        
-        if(!product){
+        if (!product) {
             return 'No se ha encontrado el producto.';
-        }else{
+        } else {
             return product;
         }
     }
-    /**
-     * @returns listado de todos los productos 
-     */
 
-    async getProducts(){
+    //Obtener el listado de productos del archivo.
+    async getProducts(path) {
         try {
-            if (existsSync(path)) {
-                const productsFile = await promises.readFile(path, 'utf-8');
+            if (fs.existsSync(path)) {
+                const productsFile = await fs.promises.readFile(path, 'utf-8');
                 return JSON.parse(productsFile);
             } else {
                 return [];
             }
         } catch (error) {
             return error;
-        } 
-    }
-
-    /**     
-     * @param {*} searchId id del producto a buscar
-     * @returns producto en caso de encontrarlo, sino error.
-     */
-
-    getProductById(searchId) {
-        const result = this.getProduct(searchId);
-
-        if (!result) {
-            return console.error('Not found.');
         }
-
-        return console.log(result);
     }
 
-
-    /**
-     * @param {*} title  @param {*} description  @param {*} price  @param {*} thumbnail  @param {*} code  @param {*} stock 
-     * @returns se agrega un nuevo producto al listado o devuelvo algun error
-     */
-
-  async  addProduct(path,title, description, price, thumbnail, code, stock) {
+    //Agregar un nuevo producto.
+    async addProduct(path, title, description, price, thumbnail, code, stock) {
         try {
-            this.path=path;
+
             if (!this.validate(title, description, price, thumbnail, code, stock)) {
                 return console.error('Debe llenar todos los campos, son obligatorios.');
             }
-    
-            if (this.existCode(code)) {
-                return console.error('El codigo ingresado ya existe.')
+
+            //El control de code repetido, solo se hace en caso de que el archivo exista.
+            if (fs.existsSync(path)) {
+
+                const existCode = await this.existCode(code, path);
+
+                if (existCode) {
+                    return console.error('El codigo ingresado ya existe.')
+                }
             }
-            
-            const products = await  this.getProducts();            
-    
+
+            const products = await this.getProducts(path);
+
             const product = {
-                id: !this.products.length ? 1 : this.products[this.products.length - 1].id + 1,
+                id: !products.length ? 1 : products[products.length - 1].id + 1,
                 title,
                 description,
                 price,
@@ -105,36 +79,111 @@ class ProductManager {
                 code,
                 stock
             }
-    
+
             products.push(product);
-            await promises.writeFile(path,JSON.stringify(products))  ; 
+            await fs.promises.writeFile(path, JSON.stringify(products));
 
         } catch (error) {
             return error;
-        }                
+        }
     }
 
-    async deleteProduct(){
-        const products = await this.getProducts();
-        const newArrayProducts= products.filter(product => product.id !== id);
-        
-        await promises.writeFile(path, JSON.stringify(products));
+    //Eliminar un objeto del archivo
+    async deleteProduct(path, id) {
+        const products = await this.getProducts(path);
+        const productIndex = products.findIndex(product => product.id === id);
+
+        if (productIndex === -1) {
+
+            return console.error('No existe el ID a eliminar.');
+
+        } else {
+
+            const newArrayProducts = products.filter(product => product.id !== id);
+
+            await fs.promises.writeFile(path, JSON.stringify(newArrayProducts));
+        }
+
     }
+
+    //Modificar parcial o total los atributos de un objeto del archivo.(sin modificar su id)
+    async updateProduct(path, id, updatedFields) {
+        try {
+            const products = await this.getProducts(path);
+            const productIndex = products.findIndex(product => product.id === id);
+
+            if (productIndex === -1) {
+                return console.error('No se ha encontrado el producto.');
+            }
+
+            const updatedProduct = { ...products[productIndex], ...updatedFields };
+
+            //forzar que por mas que venga el id como parametro a modificar, no se cambie.
+            updatedProduct.id = products[productIndex].id;
+
+            products[productIndex] = updatedProduct;
+
+            await fs.promises.writeFile(path, JSON.stringify(products));
+
+            return updatedProduct;
+        } catch (error) {
+            return error;
+        }
+    }
+
+
 }
 
 
 
 //---------------------------PRUEBAS------------------------------------------------
-const productManager1 = new ProductManager();
+async function test() {
 
-productManager1.addProduct('archivoprueba.json','producto prueba', 'esto es un producto prueba', 200, "Sin imagen", "abc123", 25);
-
-productManager1.addProduct('archivoprueba.json','producto prueba', 'esto es un producto prueba', 200, "Sin imagen", "abc1234", 25);
-
-console.log(productManager1.getProducts());
-
-// productManager1.getProductById(1);
+    const path = 'archivo1.json';
+    const productManager = new ProductManager();
 
 
+    //dar de alta productos
+    // await productManager.addProduct(path, 'producto prueba1', 'esto es un producto prueba1', 300, "Sin imagen1", "abc131", 34);
+    // await productManager.addProduct(path, 'producto prueba2', 'esto es un producto prueba2', 400, "Sin imagen2", "abc1222", 45);
 
 
+    //generar error de duplicado y guardar el que cumpla los requisitos.
+    // await productManager.addProduct(path, 'producto prueba1', 'esto es un producto prueba1', 300, "Sin imagen1", "abc131", 34);
+    // await productManager.addProduct(path, 'producto prueba3', 'esto es un producto prueba3', 400, "Sin imagen3", "abc22", 45);
+
+
+    //obtener listado de todos los productos.
+    // const products = await productManager.getProducts(path);
+
+    // console.log(products);
+
+
+
+    //obtener un objeto en especial por id
+    // const product = await productManager.getProductById(2, path);
+
+    // console.log(product);
+
+
+
+    //eliminar un objeto del archivo
+    // const deletedProduct = await productManager.deleteProduct(path, 2);
+    // const postDeleteList = await productManager.getProducts(path);
+
+    // console.log(postDeleteList);
+
+
+
+    //updetear un objeto parcial o total    
+    // const updatedFields = { id:1,title: 'titulo update', description: "update del objeto", price: 999 };
+    // const updatedProduct = await productManager.updateProduct(path, 3, updatedFields)
+
+    // console.log('updateado', updatedProduct);
+    // const postUpdateList = await productManager.getProducts(path);
+
+    // console.log(postUpdateList);
+
+}
+
+test();
